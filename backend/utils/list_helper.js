21408e9a11e964707eb29785f213d15a6556aaec
Xqni <jxesuki@gmail.com>
2025-06-22 04:30:39 -0500

    remove dummy, add favblog

--- backend/utils/list_helper.js
+++ backend/utils/list_helper.js
@@ -1,10 +1,13 @@
-const dummy = (blogs) => {
-  return 1
-}
-
 const totalLikes = (blogs) => blogs.reduce((sum, blog) => sum += blog.likes, 0)
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+
+  return blogs.reduce((max, blog) => blog.likes > max.likes ? blog : max)
+}
+
+
 module.exports = {
-  dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
