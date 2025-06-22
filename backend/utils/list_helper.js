ac9b5fe56da177aa6ee9afb9f63af7faa15e587e
Xqni <jxesuki@gmail.com>
2025-06-22 06:53:20 -0500

    create mostblogs and test

--- backend/utils/list_helper.js
+++ backend/utils/list_helper.js
@@ -1,3 +1,5 @@
+var _ = require('lodash')
+
 const totalLikes = (blogs) => blogs.reduce((sum, blog) => sum += blog.likes, 0)
 
 const favoriteBlog = (blogs) => {
@@ -6,8 +8,33 @@ const favoriteBlog = (blogs) => {
   return blogs.reduce((max, blog) => blog.likes > max.likes ? blog : max)
 }
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return null
+
+  const authorCounts = {}
+
+  blogs.forEach((blog) => {
+    authorCounts[blog.author] = (authorCounts[blog.author] || 0) + 1
+  })
+
+  let topAuthor = ''
+  let maxBlogs = 0
+
+  for (const author in authorCounts) {
+    if (authorCounts[author] > maxBlogs) {
+      topAuthor = author
+      maxBlogs = authorCounts[author]
+    }
+  }
+
+  return {
+    author: topAuthor,
+    blogs: maxBlogs
+  }
+}
 
 module.exports = {
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
