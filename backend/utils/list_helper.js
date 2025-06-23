46dc92ba6e72488eb700b68da30f4d75cad616ba
Xqni <jxesuki@gmail.com>
2025-06-23 04:10:41 -0500

    a lot of change

--- backend/utils/list_helper.js
+++ backend/utils/list_helper.js
@@ -11,27 +11,16 @@ const favoriteBlog = (blogs) => {
 const mostBlogs = (blogs) => {
   if (blogs.length === 0) return null
 
-  const authorCounts = {}
+  const grouped = _.countBy(blogs, 'author')
 
-  blogs.forEach((blog) => {
-    authorCounts[blog.author] = (authorCounts[blog.author] || 0) + 1
-  })
-
-  let topAuthor = ''
-  let maxBlogs = 0
-
-  for (const author in authorCounts) {
-    if (authorCounts[author] > maxBlogs) {
-      topAuthor = author
-      maxBlogs = authorCounts[author]
-    }
-  }
+  const topAuthor = _.maxBy(Object.keys(grouped), author => grouped[author])
 
   return {
     author: topAuthor,
-    blogs: maxBlogs
+    blogs: grouped[topAuthor]
   }
 }
+
 
 module.exports = {
   totalLikes,
