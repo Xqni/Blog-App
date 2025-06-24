f309308a5ec07454d6324ff4025acb5f9d6e327e
Xqni <jxesuki@gmail.com>
2025-06-24 06:59:03 -0500

    Add more user testing

--- backend/tests/user_api.test.js
+++ backend/tests/user_api.test.js
@@ -31,6 +31,48 @@ describe('Valid User Tests', () => {
 
     assert.strictEqual(response.body.length, helper.initialUsers.length)
   })
+
+  // check if api returns JSON formatted data
+  test('users are returned as json', async () => {
+    await api
+      .get('/api/users')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  // check if _id is deleted or not
+  // moreso a test for toJSON function at ../models/user
+  test('unique identifier is named id and not _id', async () => {
+    const response = await api.get('/api/users')
+    const users = response.body
+    users.forEach(user => {
+      // read documentation if dont get it
+      // https://nodejs.org/api/assert.html#assertokvalue-message
+
+      assert.ok(user.id)
+      assert.strictEqual(user._id, undefined)
+    })
+  })
+
+  test('adding a new user', async () => {
+    const testUser = {
+      username: 'pompumii',
+      name: 'Amy',
+      password: 'qwerty'
+    }
+
+    await api
+      .post('/api/users')
+      .send(testUser)
+      .expect(201)
+      .expect('Content-type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    assert.strictEqual(usersAtEnd.length, helper.initialUsers.length + 1)
+
+    const usernames = usersAtEnd.map(user => user.username)
+    assert(usernames.includes('pompumii'))
+  })
 })
 
 describe('Invalid User Tests', () => {
