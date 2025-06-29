de3bae035976f4e2eadd1f400e790be221438e4f
Xqni <jxesuki@gmail.com>
2025-06-29 05:19:03 -0500

    Add more playwright blog testing

--- e2etesting/tests/loginForm.spec.js
+++ e2etesting/tests/loginForm.spec.js
@@ -2,18 +2,24 @@ const { test, expect, beforeEach, describe } = require('@playwright/test')
 const helper = require('./helper')
 
 describe('Blog app', () => {
-
   // resetting the state of the db
   beforeEach(async ({ page, request }) => {
     console.log('deleting test db')
     await request.post('http://localhost:5173/api/testing/reset')
-    // create a user for the backend here
 
     console.log('creating new user')
     await request.post('http://localhost:5173/api/users', {
       data: {
         name: 'Damian',
         username: 'damianqt',
+        password: 'qwerty'
+      }
+    })
+
+    await request.post('http://localhost:5173/api/users', {
+      data: {
+        name: 'Tarik',
+        username: 'tarik',
         password: 'qwerty'
       }
     })
@@ -68,10 +74,52 @@ describe('Blog app', () => {
 
     test('a blog can be liked', async ({ page }) => {
       await helper.createBlog(page, 'test blog 3', 'damian', 'some url')
+      const blog = page.locator('.blog').filter({ hasText: '\'test blog 3\' by Damian' })
+      await blog.getByRole('button', { name: 'view' }).click()
+      await blog.getByRole('button', { name: 'like' }).click()
+    })
+
+    test('owner can delete blog', async ({ page }) => {
+      await helper.createBlog(page, 'test blog 4', 'Damian', 'some url')
+      const blog = page.locator('.blog').filter({ hasText: '\'test blog 4\' by Damian' })
+      await blog.getByRole('button', { name: 'view' }).click()
+      page.on('dialog', dialog => dialog.accept())
+      await blog.getByRole('button', { name: 'delete' }).click()
+      await expect(page.locator('.blog').filter({ hasText: '\'test blog 4\' by Damian' })).not.toBeVisible()
+    })
+
+    test('only owner sees delete button', async ({ page }) => {
+      await helper.createBlog(page, 'test blog 5', 'damian', 'some url')
+      await page.getByRole('button', { name: 'Logout' }).click()
+      await helper.loginWith(page, 'tarik', 'qwerty')
+      const blog = page.locator('.blog').filter({ hasText: '\'test blog 5\' by damian' })
+      await blog.getByRole('button', { name: 'view' }).click()
+      await expect(blog.getByRole('button', { name: 'delete' })).not.toBeVisible()
+    })
+
+    test.only('filtered list of blogs', async ({ page }) => {
+      await helper.createBlog(page, 'test blog 6', 'damian', 'some url')
+      await helper.createBlog(page, 'test blog 7', 'damian', 'some url')
+
       const blogs = page.locator('.blogs')
-      const blog = blogs.locator('.blog').filter({ hasText: '\'test blog 3\' by Damian' })
-      await blog.getByRole('button', { name: 'view'}).click()
-      await blog.getByRole('button', { name: 'like'}).click()
+      await expect(blogs.locator('.blog')).toHaveCount(2)
+
+      const blog1 = page.locator('.blog').filter({ hasText: '\'test blog 7\' by damian' })
+      await blog1.getByRole('button', { name: 'view' }).click()
+      await blog1.getByRole('button', { name: 'like' }).click()
+
+      const blog2 = page.locator('.blog').filter({ hasText: '\'test blog 6\' by damian' })
+      await blog2.getByRole('button', { name: 'view' }).click()
+
+      await page.reload()
+
+      const firstBlog = page.locator('.blog').nth(0)
+      const firstBlogContent = await firstBlog.textContent()
+      expect(firstBlogContent).toContain('test blog 7')
+
+      const secondBlog = page.locator('.blog').nth(1)
+      const secondBlogContent = await secondBlog.textContent()
+      expect(secondBlogContent).toContain('test blog 6')
     })
   })
 })
